3b9abbadfd33e9eba433894d3ff606cfbb52d0f8
JustFiesta <mateusz.bocak9@gmail.com>
2024-07-15 15:48:46 +0200

    Print hello on app start

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -35,7 +35,7 @@ import java.util.Locale;
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Change in project for new version");
+		System.out.println("Hello!");
 		SpringApplication.run(PetClinicApplication.class, args);
 	}
 
